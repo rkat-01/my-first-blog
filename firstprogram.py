78362a2d2e1279db082880fc157637cfed03e420
Raba Kata <katya.raba@gmail.com>
2015-11-21 16:14:27 +0100

    My Django Girls app, first commit

---
+++ firstprogram.py
@@ -0,0 +1,57 @@
+
+if 3 > 2 :
+	if 3 > 2:
+		print('It works!')
+if 5 > 2:
+	print('5 is indeed greater than 2')
+else:
+	print('5 is not greater then 2')
+name = 'Kata'
+if name == 'Sonja':
+	print('Hey Sonja')
+elif name == 'Kata':
+	print('Hey Kata!')
+else:
+	print('Hey anonymus!')
+volume = 57
+if volume < 20:
+	print("It's kinda quiet.")
+elif 20 <= volume < 40:
+	print("It's nice for background music")
+elif 40 <= volume < 60:
+	print("Perfect, I can hear all the details")
+elif 60 <= volume <80:
+	print("Nice for parties")
+elif 80 <= volume < 100:
+	print("A bit loud!")
+else:
+	print("My ears are hurting! :(")	
+def hi():
+    print('Hi there!')
+    print('How are you?')
+hi()   
+def hi(name):
+    if name == 'Kata':
+    	print('Hi Kata')	
+    elif name == 'Sonja':
+    	print ('Hi Sonja!')	
+    else:
+    	print ('Hi anonymus!')
+hi("Leila")
+def hi(name):
+	print('Hi ' + name + '!')
+hi("Rachel")
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Kata']
+def hi(name):
+	print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Kata']
+for name in girls:
+	hi(name)
+	print('Next girl')	
+
+
+
+
+
+	   	
